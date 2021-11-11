878a1de4adae46a69bf86aab40f87dedeced34d4
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-11-11 22:20:48 +0300

    add about

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+                dignissimos assumenda eligendi, autem sint reiciendis vel eos
+                expedita et error voluptatem perferendis mollitia numquam, iusto
+                quibusdam qui officia saepe sapiente.
+            </p>
+        </section>
+    );
+};
+
+export default About;
