75a617b8385a5084c50826896cda08307b41a4b4
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-26 22:36:59 +0300

    Updates

---
+++ precache-manifest.e40989ba945fecc0fdf6ce862e651450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4fa58eeacf61443b43b09353004627",
+    "url": "/React-Cocktail-Website/index.html"
+  },
+  {
+    "revision": "bb25fbf587a141c8ccb3",
+    "url": "/React-Cocktail-Website/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/React-Cocktail-Website/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Cocktail-Website/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb25fbf587a141c8ccb3",
+    "url": "/React-Cocktail-Website/static/js/main.8872943c.chunk.js"
+  },
+  {
+    "revision": "9793153f8c9a0ca334e4",
+    "url": "/React-Cocktail-Website/static/js/runtime-main.9c052bd8.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/React-Cocktail-Website/static/media/logo.9a3d2645.svg"
+  }
+]);
